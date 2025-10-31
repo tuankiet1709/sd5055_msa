e6d8743979b0be5ca8357e2f50c4ea195b4495d4
tuankiet1709 <tranhatuankiet1709@gmail.com>
2025-10-31 16:53:21 +0700

    update logs

--- backend/db/index.js
+++ backend/db/index.js
@@ -12,7 +12,7 @@ exports.connect = (app) => {
 
   const connectWithRetry = () => {
     mongoose.Promise = global.Promise;
-    console.log("MongoDB connection with retry");
+    console.log("MongoDB connection with retry: ", process.env.MONGODB_URI);
     mongoose
       .connect(process.env.MONGODB_URI, options)
       .then(() => {
